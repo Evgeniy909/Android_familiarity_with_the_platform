0d4bbf99fa4d2422b6797ad39eb12a0c28359aeb
Evgeniy909 <Jonny_omsk@mail.ru>
2022-03-10 23:10:29 +0500

    Урок 2. Макеты приложений 1. С этого урока будем писать приложение «Калькулятор». Выберите макет для работы с калькулятором. Обоснуйте, почему будете использовать именно этот тип макета. 2. Сверстайте главный экран калькулятора. На нём должны быть кнопки, обозначающие цифры и знаки действия: «Плюс», «Умножить», «Разделить», «Вычесть» и т. п. 3. * Добавьте фоновый рисунок для экрана калькулятора. Следите, чтобы рисунок был для общего использования. Ресурсы: PxHere, Pixabay.

--- app/src/main/java/com/example/androidfamiliaritywiththeplatform/MainActivity.java
+++ app/src/main/java/com/example/androidfamiliaritywiththeplatform/MainActivity.java
@@ -14,17 +14,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.switchLayout);
+        Button button = findViewById(R.id.buttonSwitch1);
 
         button.setOnClickListener(v ->
         {
             Intent intent = new Intent(MainActivity.this, MainActivity2.class);
             startActivity(intent);
         });
+
+        Button buttonCalc = findViewById(R.id.buttonCalc);
+
+        buttonCalc.setOnClickListener(v ->
+        {
+            Intent intent = new Intent(MainActivity.this, Calculator.class);
+            startActivity(intent);
+        });
+
     }
 
     public void ButtonClick_switch1(View view) {
         Intent intent = new Intent(MainActivity.this, MainActivity2.class);
         startActivity(intent);
     }
+
+    public void ButtonClick_calc(View view) {
+        Intent intent = new Intent(MainActivity.this, Calculator.class);
+        startActivity(intent);
+    }
+
 }
